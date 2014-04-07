e36b3544dfd5b59fa1901a8727c58988ba32fd1d
Jon Park <jon.park@thap.co.uk>
2014-04-07 11:52:25 +0100

    We don't need to be so specific of prerequisites Taking care of PHP upgrade as part of package install Removing unused packages Removing PHP ini nuke

--- vagrant/modules/php/manifests/init.pp
+++ vagrant/modules/php/manifests/init.pp
@@ -8,8 +8,6 @@ class php {
     $packages = [
             'php5-cli',
             'php5-dev',
-            'php-apc',
-            'php-pear',
             'php5-intl',
             'php5-mysql',
             'php5-mcrypt',
@@ -17,17 +15,6 @@ class php {
         ]
 
     package { $packages:
-        ensure  => present,
-        require => Package['php5'],
+        ensure  => latest,
     }
-
-    # Copy over our default php.ini file
-    # file { 'php-ini':
-    #     ensure  => present,
-    #     path    => '/etc/php5/apache2/php.ini',
-    #     owner   => 'root',
-    #     group   => 'root',
-    #     content => template('php/php.erb'),
-    #     require => [ Package['php5'], ],
-    # }
 }
